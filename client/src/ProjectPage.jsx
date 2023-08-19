180aa300720d4b7cb2805097f5ac2cd760fcf42b
BradySBaker <bradysamuelbaker@gmail.com>
2023-08-19 11:21:23 -0700

    Added preload images

--- client/src/ProjectPage.jsx
+++ client/src/ProjectPage.jsx
@@ -7,17 +7,38 @@ import styles from './cssModules/projectPage.module.css';
 const laptopPositionMap = { 780: 1, 685: 3, 575: 5, 485: 7, 380: 9, 280: 11 };
 const phonePositionMap = { 780: 1, 685: 4, 575: 6, 485: 8, 380: 10, 280: 13 };
 
+const toyShareContributions = ['Map Feature', 'Individual Toy Page', 'Toy Distance', 'Toy Locations', 'All Toy Backend Routes'];
+
 const ProjectPage = () => {
   return (
     <div id={styles.widget}>
       <h1>Projects</h1>
       <div id={styles.underline}/>
-      <h3>Below, are some of my finished projects</h3>
-      <div id={styles.laptop}>
-      <ScrollAnimator src={'laptop'} positionMap={laptopPositionMap} width={'55vw'}/>
+      <h3>Below are some of my finished projects</h3>
+      <div className={styles.project}>
+        <div id={styles.phone} style={{ transform: 'translateY(-20vw);' }}>
+          <ScrollAnimator src={'phone'} positionMap={phonePositionMap} width={'28vw'}/>
+        </div>
+        <div id={styles['phone-description']}>
+          <h2>Toy Share</h2>
+          <p>
+          ToyShare is a comprehensive application developed by a collaborative team, enabling users to effortlessly lend their toys to others or borrow playthings from fellow users.
+          </p>
+          <h4>My Contributions</h4>
+          <div id={styles['contribution-list']}>
+            {toyShareContributions.map((curContr) => {
+              return (
+                <div key={curContr} className={styles.contributions}>{curContr}</div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      <div id={styles.phone} style={{ transform: 'translateY(-20vw);' }}>
-        <ScrollAnimator src={'phone'} positionMap={phonePositionMap} width={'28vw'}/>
+      <div className={styles.project}>
+        <div id={styles.laptop}>
+        <ScrollAnimator src={'laptop'} positionMap={laptopPositionMap} width={'55vw'}/>
+        </div>
+        <h2></h2>
       </div>
   </div>
   );
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { createRoot } from 'react-dom/client';
 
 import HomePage from './HomePage.jsx';
@@ -7,13 +7,47 @@ import TopBar from './smallWidgets/TopBar.jsx';
 import ProjectPage from './ProjectPage.jsx';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const images = [];
+    const imageGroups = { laptop: 11, phone: 13 };
+    const max = 13;
+    for (let i = 0; i < max; i++) {
+      for (const imageGroup in imageGroups) {
+        const count = imageGroup[imageGroups];
+        if (i < count) {
+          const img = new window.Image();
+          img.src = './images/' + imageGroup + i + '.png';
+          images.push(img);
+        }
+      }
+    }
+
+    Promise.all(images.map(img => {
+      return new Promise((resolve, reject) => {
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    })).then(() => {
+      setLoading(false);
+      console.log('loading finished');
+    }).catch(error => {
+      console.error('Error preloading images:', error);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div id="app" className="w-full">
-      <TopBar />
-      <HomePage />
-      <ProjectPage />
-      <div><a href="https://www.freepik.com/free-vector/home-screen-concept-illustration_11906660.htm#query=phone&position=0&from_view=author">Image by storyset</a> on Freepik</div>
-      <div>Laptop Art from GrafixM</div>
+      {!loading
+        ? <>
+        <TopBar />
+        <HomePage />
+        <ProjectPage />
+        <div><a href="https://www.freepik.com/free-vector/home-screen-concept-illustration_11906660.htm#query=phone&position=0&from_view=author">Image by storyset</a> on Freepik</div>
+        <div>Laptop Art from GrafixM</div>
+      </>
+        : null}
     </div>
   );
 };
