0f5cf41fb61b8c35b7b93711463115a1543c06d3
BradySBaker <bradysamuelbaker@gmail.com>
2023-08-24 21:31:45 -0700

    Website no longer reloads on resize, cleaned up code, added styling

--- client/src/smallWidgets/TopBar.jsx
+++ client/src/smallWidgets/TopBar.jsx
@@ -2,9 +2,9 @@ import React, { useRef, useEffect, useState } from 'react';
 
 import styles from '../cssModules/smallWidgetsStyles.module.css';
 
-const buttons = [ 'Home', 'Projects', 'About', 'Contact'];
+const buttons = ['Home', 'Projects', 'About', 'Contact'];
 
-const barPositionsWidth = {Home: {}, Projects: {}, About: {}, Contact: {}};
+const barPositionsWidth = { Home: {}, Projects: {}, About: {}, Contact: {} };
 const sectionPositions = { Home: 0 };
 
 let webHeight = 0;
@@ -15,18 +15,17 @@ const scrollTo = (name) => {
   window.scrollTo({ top: sectionPositions[name], behavior: 'smooth' });
 };
 
-const reload = () => {
-  location.reload();
-};
-
 const TopBar = () => {
-  const targetElementRef = useRef(null);
+  const buttonRefs = useRef({});
   const [buttonElements, setButtonElements] = useState([]);
-  const [barPositionWidth, setBarPosWidth] = useState({pos: 0, width: 50});
+  const [barPositionWidth, setBarPosWidth] = useState({ pos: 0, width: 50 });
+
+  const [refresh, setRefresh] = useState(false);
+
   const setPositions = () => {
     webHeight = document.getElementById('app').offsetHeight;
 
-    if (buttonElements.length < 1  &&  !barPositionsWidth.Home.pos) {
+    if (buttonElements.length < 1 && !barPositionsWidth.Home.pos) {
       return;
     }
 
@@ -35,53 +34,64 @@ const TopBar = () => {
     sectionPositions.Projects = document.getElementById('projects').getBoundingClientRect().top;
     sectionPositions.About = document.getElementById('about').getBoundingClientRect().top;
     sectionPositions.Contact = document.getElementById('contact').getBoundingClientRect().top;
-    const elements = targetElementRef.current.children;
-    for (let i = 0; i < elements.length; i++) {
-    const curName = elements[i].id;
-     barPositionsWidth[curName].pos = elements[i].getBoundingClientRect().left;
-     barPositionsWidth[curName].width = elements[i].offsetWidth;
+
+    for (const name in buttonRefs.current) {
+      const buttonElement = buttonRefs.current[name].current;
+      if (buttonElement) {
+        barPositionsWidth[name].pos = buttonElement.getBoundingClientRect().left;
+        barPositionsWidth[name].width = buttonElement.offsetWidth;
+      }
     }
     setBarPosWidth(barPositionsWidth.Home);
+  };
+
+  const handleResize = () => {
+    setPositions();
+    setRefresh((prevRefresh) => !prevRefresh);
   };
 
   useEffect(() => {
     webHeight = document.getElementById('app').offsetHeight;
     const elements = [];
     buttons.forEach((name) => {
-      elements.push(<div className={styles['top-buttons']} id={name} key={name} onClick={() => scrollTo(name)}>{name}</div>);
+      const buttonRef = React.createRef();
+      elements.push(<div className={styles['top-buttons']} ref={buttonRef} id={name} key={name} onClick={() => scrollTo(name)}>{name}</div>);
       setButtonElements(elements);
-    })}, []);
+      buttonRefs.current[name] = buttonRef;
+    });
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition = window.scrollY;      
-      if (scrollPosition + window.innerHeight + 5>= webHeight) {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition + window.innerHeight + 5 >= webHeight) {
         setBarPosWidth(barPositionsWidth.Contact);
         return;
       }
-      if (Math.abs(scrollPosition - prevScrollPos) < 50) { //For performance
+      if (Math.abs(scrollPosition - prevScrollPos) < 50) { // For performance
         return;
       }
       prevScrollPos = scrollPosition;
       let min;
       let minKey;
       for (const key in sectionPositions) {
-        let distance = Math.abs(sectionPositions[key] - scrollPosition);
+        const distance = Math.abs(sectionPositions[key] - scrollPosition);
         if (distance < min || min === undefined) {
           min = distance;
           minKey = key;
         }
       }
       if (min !== undefined) {
+        console.log(buttonRefs.current);
         setBarPosWidth(barPositionsWidth[minKey]);
       }
     };
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', reload);
+    window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('resize', reload);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
@@ -89,11 +99,11 @@ const TopBar = () => {
 
   return (
     <div id={styles['top-element']}>
-      <div id={styles['top-bar']} ref={targetElementRef}>
+      <div id={styles['top-bar']}>
         {buttonElements}
       </div>
       <svg id={styles['scroll-bar']}>
-          <rect x={barPositionWidth.pos + 'px'} id={styles.bar} width={barPositionWidth.width + 'px'} fill='#0073cf' rx=".4rem" ry=".4rem"/>
+          <rect x={barPositionWidth.pos} id={styles.bar} width={barPositionWidth.width} fill='#0073cf' />
       </svg>
   </div>
   );
