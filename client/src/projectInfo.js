0f07b1c62f93cdebd188c3fddd8d06d71be837fe
BradySBaker <bradysamuelbaker@gmail.com>
2023-08-26 18:39:57 -0700

    Small fix

--- client/src/projectInfo.js
+++ client/src/projectInfo.js
@@ -8,11 +8,11 @@ export default {
   fec: {
     contributions: ['Review List', 'Lazy Loading', 'Sort Reviews', 'Filter Reviews', 'Post Image Review', 'Review API fetch', { text: 'Product MicroService', style: { backgroundColor: 'rgba(255, 0, 0, 0.466)' } }],
     desc: 'Grace Outfitters is a web app displaying relevant information for a single product in the catalog of an online retailer. It is composed of of Product Overview, Ratings & Reviews, Questions & Answers, and Related Items. The API is also composed of the main 3 microservices',
-    libraries: ['Node.js', 'Express', 'PostgreSQL', 'Redis', 'NGINX', 'Jest', 'K6', 'Docker', 'EC2', 'React-Redux', 'Jest', 'EC2/S3', 'Axios', 'Babel', 'Webpack']
+    libraries: ['Node.js', 'Express', 'PostgreSQL', 'Redis', 'NGINX', 'Jest', 'K6', 'Docker', 'EC2', 'React-Redux', 'Jest', 'EC2/S3', 'Axios', 'Babel', 'Webpack', 'css-loader/style-loader']
   },
   ledCanvas: {
     contributions: [{ text: 'Solo Project', style: { backgroundColor: 'rgba(255, 0, 0, 0.466)' } }],
     desc: 'LED Canvas is an interactive pixel art device controlled via Bluetooth. There is a mobile-first web app to control the device, although it works on desktop. The device itself consists of an Arduino controller, microphone, Bluetooth module, 16x16 LED grid, and a custom 3D-printed casing.',
-    libraries: ['Arduino', 'React', 'P5ble', 'FastLED', 'React-Color']
+    libraries: ['Arduino', 'React', 'P5ble', 'FastLED', 'React-Color', 'babel', 'css-loader/style-loader']
   }
 };
